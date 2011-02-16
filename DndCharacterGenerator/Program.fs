01336e89cf57347fb7daeb8afdbab585a7a68aa7
Seth Dodson <seth_dodson@hotmail.com>
2011-02-16 17:40:32 -0500

    Initial commit

---
+++ DndCharacterGenerator/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.net
+
+
+[<EntryPoint>]
+let main args =
+    printfn "Arguments passed to function : %A" args
+    // Return 0. This indicates success.
+    0
+
+
