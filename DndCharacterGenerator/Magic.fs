631b95d6d61b60f39b0035440aa9c6ee4167431c
Seth Dodson <seth_dodson@hotmail.com>
2011-04-01 16:51:40 -0400

    committing before pushing to github

---
+++ DndCharacterGenerator/Magic.fs
@@ -0,0 +1,39 @@
+﻿module Magic
+
+type SpellType = WizardSpell | PriestSpell
+
+type School = 
+    | Illusion
+    | Enchantment
+    | Conjuration
+    | Abjuration
+    | Necromancy
+    | Invocation
+    | GreaterDivination
+    | Alteration
+    
+type Sphere = string
+
+type SpellRange = 
+    | Self
+    | Touch
+    | Distance of int
+    
+type SpellComponent = 
+    | Verbal
+    | Somatic
+    | Material of string
+    
+type SpellDuration = 
+    | RoundsPerLevel of int
+    | Variable of (unit -> int)
+
+type WizardSpell = 
+    { Name: string
+      School: School
+      Range: SpellRange
+      Components: SpellComponent list
+      Duration: SpellDuration
+      
+      
+      
