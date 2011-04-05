5f574cdfa5ebf91e3e532ed0f40de26a896c57a3
Seth Dodson <seth_dodson@hotmail.com>
2011-04-04 20:28:11 -0400

    Began preliminary work on interface.

--- Interface/Interface/BasicStatsView.xaml.cs
+++ Interface/Interface/BasicStatsView.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 namespace Interface
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for BasicStatsView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class BasicStatsView : UserControl
     {
-        public MainWindow()
+        public BasicStatsView()
         {
             InitializeComponent();
         }
---
+++ Interface/Interface/CharacterViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Interface
+{
+    public class CharacterViewModel : INotifyPropertyChanged
+    {
+        private CharacterGenerator.CharacterModel _characterModel;
+
+        public CharacterGenerator.CharacterModel CharacterModel
+        {
+            get { return _characterModel; }
+            set
+            {
+                if (value == _characterModel) return;
+                _characterModel = value;
+                RaisePropertyChanged("CharacterModel");
+            }
+        }
+
+        public string Name { get; set; }
+
+        public CharacterViewModel()
+        {
+            _characterModel = CharacterGenerator.getCharacter(1);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
