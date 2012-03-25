7984dd6e8161eeca03cae898557abcc6a6a38a87
Seth Dodson <seth_dodson@hotmail.com>
2012-03-25 13:59:29 -0400

    added console

---
+++ DndCharacterGenerator/Console/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.net
+
+open CharacterGenerator
+open System
+
+Console.WriteLine("Character Generator")
+
+let generateCharacters = 
+    let mutable level = "z"
+    while 
